035b55f2c25718336129c7b21054f08bb79716d2
Sandy <sandra_hakufu@hotmail.com>
2026-04-09 11:48:52 -0600

    add reusable componets

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,37 @@
-<script setup lang="ts"></script>
-
 <template>
   <section class="hero">
     <div class="hero-body">
-      <p class="title">Hero title</p>
-      <p class="subtitle">Hero subtitle</p>
+      <p class="title">Películas del Studio Ghibli</p>
+      
+      <div class="columns is-multiline">
+        <div class="column is-4">
+          <Input 
+            placeholder="Buscar película..."
+            v-model="search"
+           />
+        </div>
+        <div class="column is-12">
+          <div class="columns">
+            <div class="column is-3">
+              <Card />
+            </div>
+            <div class="column is-3">
+              <Card />
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   </section>
 </template>
 
+<script setup lang="ts">
+import { ref } from 'vue' 
+import Input from './components/input.vue'
+import Card from './components/card.vue'
+
+const search = ref('')
+</script>
+
 <style scoped>
-  
 </style>
---
+++ src/components/input.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="field">
+    <p class="control has-icons-right">
+      <input
+        class="input"
+        type="email"
+        :placeholder="props.placeholder"
+        @input="emits('update:modelValue', $event.target.value)"
+      />
+      <span class="icon is-small is-right">
+        <i class="fa-solid fa-magnifying-glass"></i>
+      </span>
+    </p>
+  </div>
+</template>
+
+<script setup lang="ts">
+// TODO: Add logic for input validation and handling user input
+const props = defineProps<{
+    placeholder?: string
+    modelValue?: string
+}>()
+
+const emits = defineEmits<{
+    (event: 'update:modelValue', value: string): void
+}>()
+</script>
